5ee243bd2922ae202965eaf8b6115cf50bae9a4f
Nawantorn113 <stu6512732113@sskru.ac.th>
2023-08-03 11:27:40 +0700

    commit

--- my_web/app/views.py
+++ my_web/app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "name":"khim"
+    }
+
+    # context ={
+    #   'students' : students,
+    # }
+    students = models.Students.objects.all()
+
+    context ['students'] = students
+
+    return render(request, 'main.html',context)
