2210a1fc948cf6802474c5ab5197df3c0c4f7a12
Nawantorn113 <stu6512732113@sskru.ac.th>
2023-08-03 10:16:29 +0700

    my web

--- my_web/app/admin.py
+++ my_web/app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Student
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+        list_display = ('std_id','name', 'lastname','phone')
+        search_fields =('name','prefix','lastname','phone','std_id')
--- my_web/app/models.py
+++ my_web/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+prefix_choices =(
+    (1,"นาย"),
+    (2,"นางสาว"),
+    (3,"นาง"),
+)
+
+class Student(models.Model):
+    std_id =    models.IntegerField()
+    prefix = models.IntegerField(choices=prefix_choices,    default=1)
+    name=       models.CharField(max_length=255)
+    lastname=   models.CharField(max_length=255)
+    phone =     models.CharField(max_length=255)
+    address=    models.TextField()
+    
+
+    class Meta:
+        verbose_name = 'student'
+        verbose_name_plural = 'student'
+
+    def __str__(self):
+        return self.name + " " + self.lastname
